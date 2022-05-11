b3b3892499d1e09fb18f16d3935c6504a8e7aad5
uvsq22104458 <many.afaghpour@ens.uvsq.fr>
2022-05-11 14:30:59 +0200

    Added documentation and cleaned the code

--- README.md
+++ README.md
@@ -1,14 +1,36 @@
-###################################################
-
-Group BI 2
+# **Ant of Langton project**
+>**Group BI 2
 Bastien COMBES
 Adrien AUGIS
 Many AFAGHPOUR
-Perrine ILARY
-https://github.com/uvsq22104458/ant_of_langton_project
-###################################################
+Perrine ILARY**
+>>[Ant of Langton project](https://github.com/uvsq22104458/ant_of_langton_project)
+------------------------------------------------------------------
+![Ant of Langton](https://i.ibb.co/Gkz1t9J/2022-05-11-12-49-56.gif 'Ant')
 
-# ant_of_langton_project
-The Langton Ant is a cellular automaton composed of a grid of two dimensions and an ant.
-For more details: https://en.wikipedia.org/wiki/Langton%27s_ant
+> 💠 ***```The Langton Ant is a cellular automaton composed of a grid of two dimensions and an ant.```*** 💠
 
+## **Rules**
+The rules for the ant for a single iteration:
+
+* If the ant is at a white square, rotate 90 degrees right, if it's at a black square, rotate left.
+* Change the color of the square in the ant's current position (black turns to white, white turns to black).
+* Move forward one space.
+* Repeat these steps for each iteration.
+
+## **Features**
+* Button Play/Pause : This allows the steps to be carried out as long as it is active.
+* Button Next : This allows you to do one iteration at a time.
+* Button Back : This allows you to go back in iteration.
+* Button Save : Saves a current instance to a file.
+* Button Load : Opens a registered instance.
+* Button quit : Quit the program.
+* Entry to change the delay between each iteration : Select a number and click on 'Set', its allow you to change the delay. Max value is 200.
+* Entry to change the scale of the grid : Select a number and clic on 'Set', its allow you to change the scale of the grid. Be aware, it reset the grid. Max value is 200.
+* Iteration counter : In the grid, count each iteration. It can increase or decrease using Button back.
+* Creates an ant on click and set the clicked square to red.
+
+## **Futher improvements**
+The rules can be modified to integrate multiple colors that affects the ants movements.
+
+[Full explanation here](http://www.thealmightyguru.com/Wiki/index.php?title=Langton%27s_ant#Additional_Rules)
--- ant_of_langton.py
+++ ant_of_langton.py
@@ -1,4 +1,4 @@
-####################
+#####################
 # importing modules #
 #####################
 import tkinter as tk
@@ -6,6 +6,7 @@ from tkinter import filedialog as fd
 import os
 from os.path import exists
 from ast import literal_eval
+import re
 ######################
 # Defining constants #
 ######################
@@ -17,56 +18,52 @@ MAX_DELAY = 1000
 UP, DOWN, LEFT, RIGHT = 0, 1, 2, 3
 ROTATE_LEFT = (LEFT, RIGHT, DOWN, UP)
 ROTATE_RIGHT = (RIGHT, LEFT, UP, DOWN)
-RED, GREEN, YELLOW, CYAN = 1, 2, 3, 0
-ANT_COLORS = (RED, GREEN, YELLOW, CYAN)
-ANT_COULEURS = ('RED', 'GREEN', 'YELLOW', 'CYAN')
 MOVE = ((-1, 0), (1, 0), (0, -1), (0, 1))
 COLORS = ('white', 'black')
 WHITE, BLACK = 0, 1
 ####################
 # Global variables #
 ####################
+# grid
 grid_scale = 50
 offset_grid = WIDTH//grid_scale
 grid = [[WHITE]*grid_scale for _ in range(grid_scale)]
 grid_GUI = [[WHITE]*grid_scale for _ in range(grid_scale)]
 paused = True
-grille_active = True
 delay_value = 100
 iteration_counter = 0
 ants = []
+# tkinter
 root = tk.Tk()
 root.title("Cellular automaton: Ant Of Langdon")
 canvas = tk.Canvas(root, width=WIDTH, height=HEIGHT)
 text_iteration_counter = tk.IntVar(
     root, value=iteration_counter)
-# Directory creation
-directory_name = 'Saves'
-path = os.path.join(os.getcwd(), directory_name)
-# File creation
-file_counter = 0
-file_name = f'save{file_counter}.txt'
 #############
 # Functions #
 #############
 
 
 def create_ant(y, x):
-    '''Ant of Langton, it can moves on the grid'''
+    '''Creates a new ant with coordinates and a direction
+    then add it to the list of ants'''
     global ants
-    ant = (y, x, RIGHT,RED)
+    ant = (y, x, RIGHT)
     ants.append(ant)
 
 
 def rules():
-    '''Règles de base'''
-    global ants, grid, grid_scale, iteration_counter, text_iteration_counter
+    '''Rules that dictate how the ants will move
+    depending on the color of the square they are currently in'''
+    global ants, grid, grid_scale, iteration_counter
+    previous_ant = []
     for i, ant in enumerate(ants):
-        y, x, direction, couleur = ant
+        y, x, direction = ant
         if grid[y][x] == BLACK:
             new_direction = ROTATE_LEFT[direction]
             grid[y][x] = WHITE
-            update_GUI(y, x, 'white')
+            if not (y, x) in previous_ant:
+                update_square_color(y, x, 'white')
             offset_y, offset_x = MOVE[new_direction]
             new_y, new_x = y+offset_y, x+offset_x
             if new_y >= grid_scale:
@@ -77,12 +74,14 @@ def rules():
                 new_x = 0
             if new_x < 0:
                 new_x = grid_scale-1
-            new_color = ANT_COLORS[couleur]
-            update_GUI(new_y, new_x,ANT_COULEURS[new_color])
+            update_square_color(new_y, new_x, 'red')
+            previous_ant.append((new_y, new_x))
+            ants[i] = (new_y, new_x, new_direction)
         else:
             new_direction = ROTATE_RIGHT[direction]
             grid[y][x] = BLACK
-            update_GUI(y, x, 'black')
+            if not (y, x) in previous_ant:
+                update_square_color(y, x, 'black')
             offset_y, offset_x = MOVE[new_direction]
             new_y, new_x = y+offset_y, x+offset_x
             if new_y >= grid_scale:
@@ -93,85 +92,20 @@ def rules():
                 new_x = 0
             if new_x < 0:
                 new_x = grid_scale-1
-            new_color = ANT_COLORS[couleur]
-            update_GUI(new_y, new_x, ANT_COULEURS[new_color])
-        ants[i] = (new_y, new_x, new_direction, new_color)
+            update_square_color(new_y, new_x, 'red')
+            previous_ant.append((new_y, new_x))
+            ants[i] = (new_y, new_x, new_direction)
         iteration_counter += 1
-        update_value_counter()
-
-
-def update_value_counter():
-    ''''''
-    text_iteration_counter.set(iteration_counter)
-
-
-def iteration():
-    ''''''
-    global id_after, delay_value
-    rules()
-    id_after = canvas.after(delay_value, iteration)
-
-
-def draw_GUI(grille_act):
-    '''Create a grid in the canvas'''
-    global grid_GUI, grid
-    for i in range(grid_scale):
-        y0 = offset_grid*i
-        y1 = (offset_grid*i) + offset_grid
-        for j in range(grid_scale):
-            grid_GUI[i][j] = canvas.create_rectangle(
-                (offset_grid*j, y0), ((offset_grid*j)+offset_grid, y1),
-                fill=COLORS[grid[i][j]], outline=grille_act)
-
-
-def update_GUI(y, x, color):
-    ''''''
-    canvas.itemconfig(grid_GUI[y][x], fill=color)
-
-
-def clicked(event):
-    ''''''
-    global offset_grid, ants
-    x, y = int(event.x // offset_grid), int(event.y // offset_grid)
-    if any((True for ant in ants if ant[0] == y and ant[1] == x)):
-        return
-    create_ant(y, x)
-    update_GUI(y, x, 'red')
-
-
-def play():
-    '''Switch between Play or Pause if the button is clicked'''
-    global paused, id_after
-    if paused:
-        play_button.config(text="Pause")
-        iteration()
-    else:
-        play_button.config(text="Play")
-        canvas.after_cancel(id_after)
-    paused = not paused
-
-def activ_grid():
-    global grille_active
-    if grille_active:
-        outline='#fb0'
-    else:
-        outline=''
-    draw_GUI(outline)
-    grille_active = not grille_active
-
-def next():
-    '''A button'''
-    global paused
-    if not paused:
-        return
-    rules()
+        update_iteration_counter()
 
 
 def back():
-    '''Reverse rule'''
-    global ants, grid, grid_scale, paused, iteration_counter, text_iteration_counter
+    '''Rules to return to a previous iteration'''
+    global ants, grid, grid_scale, paused, iteration_counter
+    global text_iteration_counter
     if not paused or iteration_counter == 0:
         return
+    previous_ant = []
     for i, ant in enumerate(ants):
         y, x, direction = ant
         offset_y, offset_x = MOVE[direction]
@@ -184,37 +118,100 @@ def back():
             new_x = 0
         if new_x < 0:
             new_x = grid_scale-1
-        update_GUI(y, x, COLORS[grid[y][x]])
+        if not (y, x) in previous_ant:
+            update_square_color(y, x, COLORS[grid[y][x]])
         if grid[new_y][new_x] == BLACK:
             new_direction = ROTATE_LEFT[direction]
             grid[new_y][new_x] = WHITE
-            update_GUI(new_y, new_x, 'red')
+            update_square_color(new_y, new_x, 'red')
         else:
             new_direction = ROTATE_RIGHT[direction]
             grid[new_y][new_x] = BLACK
-            update_GUI(new_y, new_x, 'red')
+            update_square_color(new_y, new_x, 'red')
         ants[i] = (new_y, new_x, new_direction)
         iteration_counter -= 1
-        update_value_counter()
+        update_iteration_counter()
 
 
-def save():
-    '''A button'''
-    global file_counter, file_name, grid, grid_scale, ants, offset_grid, iteration_counter
+def simulation_loop():
+    '''Run the rules in a loop with a delay'''
+    global id_after, delay_value
+    rules()
+    id_after = canvas.after(delay_value, simulation_loop)
+
+
+def draw_GUI():
+    '''Create a grid in the canvas with colored rectangles.'''
+    global grid_GUI, grid
+    for i in range(grid_scale):
+        y0 = offset_grid*i
+        y1 = (offset_grid*i) + offset_grid
+        for j in range(grid_scale):
+            grid_GUI[i][j] = canvas.create_rectangle(
+                (offset_grid*j, y0), ((offset_grid*j)+offset_grid, y1),
+                fill=COLORS[grid[i][j]], outline='')
+
+
+def update_iteration_counter():
+    '''Display in the canvas the updated iteration counter.'''
+    text_iteration_counter.set(iteration_counter)
+
+
+def update_square_color(y, x, color):
+    '''Update the color of a square with given coordinates in the GUI'''
+    canvas.itemconfig(grid_GUI[y][x], fill=color)
+
+
+def clicked(event):
+    '''Creates an ant on click and set the clicked square to red'''
+    global offset_grid, ants
+    x, y = int(event.x // offset_grid), int(event.y // offset_grid)
+    if any((True for ant in ants if ant[0] == y and ant[1] == x)):
+        return
+    create_ant(y, x)
+    update_square_color(y, x, 'red')
+
+
+def play():
+    '''Switch between Play or Pause if the button is clicked. When is Play,
+    start the iteration of ants'''
+    global paused, id_after
+    if paused:
+        play_button.config(text="Pause")
+        simulation_loop()
+    else:
+        play_button.config(text="Play")
+        canvas.after_cancel(id_after)
+    paused = not paused
+
+
+def next():
+    '''Calls one time the rules function if the game is paused'''
+    global paused
+    if not paused:
+        return
+    rules()
+
+
+def save(path):
+    '''Save the current values of the simulation in a file'''
+    global grid, grid_scale, ants, offset_grid, iteration_counter
     if not exists(path):
         os.mkdir(path)
-    for files in os.listdir(path):
-        if files == file_name:
-            file_counter += 1
-            file_name = f'save{file_counter}.txt'
+    nb_files = sorted([int(n)
+                       for n in re.findall(r"\d+", ''.join(os.listdir(path)))])
+    file_name = f'save{len(nb_files)}.txt'
+    for i, n in enumerate(nb_files):
+        if i != n:
+            file_name = f'save{i}.txt'
     with open(os.path.join(path, file_name), 'w') as file:
-        file.write(
-            f'{grid_scale}\n{iteration_counter}\n{offset_grid}\n{ants}\n{grid}')
+        file.write(f'{grid_scale}\n{iteration_counter}\n')
+        file.write(f'{offset_grid}\n{ants}\n{grid}')
         file.close()
 
 
-def load():
-    '''A button'''
+def load(path):
+    '''Load the values of a selected file on the screen'''
     global grid, grid_scale, ants, offset_grid, grid_GUI, iteration_counter
     lines = []
     load_file = fd.askopenfilename(initialdir=path,
@@ -229,46 +226,53 @@ def load():
             content = file.readlines()
         for line in content:
             lines.append(line)
-        print(lines)
         grid_scale = int(lines[0])
         grid_GUI = [[WHITE]*grid_scale for _ in range(grid_scale)]
         iteration_counter = int(lines[1])
         offset_grid = int(lines[2])
         ants = literal_eval(lines[3])
         grid = literal_eval(lines[4])
-        update_value_counter()
-        draw_GUI('')
+        update_iteration_counter()
+        draw_GUI()
+        for ant in ants:
+            update_square_color(ant[0], ant[1], 'red')
 
 
-def delay(int):
-    '''A button'''
+def delay():
+    '''Gets the delay between iterations'''
     global text_entry_delay, delay_value
     delay_value = text_entry_delay.get()
     if delay_value > MAX_DELAY:
         delay_value = MAX_DELAY
-    text_entry_delay.set(int)
+    text_entry_delay.set(0)
 
 
-def entry_grid_scale(int):
-    '''Changes the scale of the grid'''
+def entry_grid_scale():
+    '''Changes and resets the grid scale'''
     global text_entry_scale, offset_grid, grid_scale, grid, grid_GUI, ants
+    global iteration_counter
     new_scale = text_entry_scale.get()
     if new_scale > MAX_SCALE:
         grid_scale = MAX_SCALE
     else:
         grid_scale = new_scale
+    # reset variables
     offset_grid = WIDTH//grid_scale
     grid = [[WHITE]*grid_scale for _ in range(grid_scale)]
     grid_GUI = [[WHITE]*grid_scale for _ in range(grid_scale)]
     ants = []
-    draw_GUI('')
-    text_entry_scale.set(int)
+    iteration_counter = 0
+    update_iteration_counter()
+    draw_GUI()
+    text_entry_scale.set(0)
 
 
 def GUI_widgets():
-    '''Graphic interface using tkinter with buttons and canvas'''
-    global text_entry_scale, text_entry_delay, play_button, save_button, iteration_counter
-
+    '''Graphic interface using tkinter with buttons, canvas,
+    Labelframe, Intvar, entry, bind and the widget placement'''
+    global text_entry_scale, text_entry_delay, play_button, save_button
+    global iteration_counter
+    path = os.path.join(os.getcwd(), 'Saves')
     # Labelframe
     labelframe_delay = tk.LabelFrame(root,
                                      text='Changes the delay of the ants')
@@ -278,19 +282,18 @@ def GUI_widgets():
     quit_button = tk.Button(root, text='Quit', command=root.quit)
     play_button = tk.Button(root, text='Play', command=play)
     next_button = tk.Button(root, text='Next', command=next)
-    save_button = tk.Button(root, text='Save', command=save)
-    load_button = tk.Button(root, text='Load', command=load)
+    save_button = tk.Button(root, text='Save', command=lambda: save(path))
+    load_button = tk.Button(root, text='Load', command=lambda: load(path))
     back_button = tk.Button(root, text='Back', command=back)
-    grid_button = tk.Button(root, text='grille', command=activ_grid)
     scale_button = tk.Button(
-        labelframe_scale, text='Set', command=lambda: entry_grid_scale(0))
+        labelframe_scale, text='Set', command=entry_grid_scale)
     delay_button = tk.Button(
-        labelframe_delay, text='Set', command=lambda: delay(0))
+        labelframe_delay, text='Set', command=delay)
     # IntVar
     text_entry_scale = tk.IntVar()
     text_entry_delay = tk.IntVar()
     # Label
-    label_interation_counter = tk.Label(
+    label_iteration_counter = tk.Label(
         root, textvariable=text_iteration_counter)
     # Entry
     delay_entry = tk.Entry(labelframe_delay, textvariable=text_entry_delay)
@@ -304,13 +307,12 @@ def GUI_widgets():
     load_button.grid(row=0, column=5)
     delay_entry.grid(row=0, column=6)
     delay_button.grid(row=0, column=7)
-    grid_button.grid(row=0, column=8)
-    scale_text.grid(row=0, column=9)
-    labelframe_scale.grid(row=0, column=10)
+    scale_text.grid(row=0, column=8)
+    labelframe_scale.grid(row=0, column=9)
     labelframe_delay.grid(row=0, column=7)
     scale_button.grid(row=0, column=9)
     quit_button.grid(row=0, column=15)
-    label_interation_counter.grid(row=1, column=1)
+    label_iteration_counter.grid(row=1, column=1)
     # Bind
     canvas.bind('<Button-1>', clicked)
 
@@ -320,8 +322,8 @@ def GUI_widgets():
 
 
 def main():
-    '''Main program with tkinter instructions'''
-    draw_GUI('')
+    '''Main functions and variables'''
+    draw_GUI()
     GUI_widgets()
     root.mainloop()
 
